2a0bd27ffaa32d37b1e60aeba44fced08680999f
MarcAgate <agate.marc@gmail.com>
2018-10-18 20:54:11 +0200

    Preparing version 0.3.0 with single props file

--- src/main/java/io/bdrc/auth/AuthProps.java
+++ src/main/java/io/bdrc/auth/AuthProps.java
@@ -10,20 +10,20 @@ import org.slf4j.LoggerFactory;
 
 /*******************************************************************************
  * Copyright (c) 2018 Buddhist Digital Resource Center (BDRC)
- * 
+ *
  * If this file is a derivation of another work the license header will appear
  * below; otherwise, this work is licensed under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.
- * 
+ *
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * 
+ *
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
@@ -32,21 +32,18 @@ public class AuthProps {
 
     static Properties authProp;
 
-    public final static Logger log = LoggerFactory.getLogger(BdrcJwks.class.getName());
+    public final static Logger log = LoggerFactory.getLogger(AuthProps.class.getName());
 
-    static {
-        final InputStream input = AuthProps.class.getClassLoader().getResourceAsStream("auth.properties");
+    public static void init(String propFile) {
         try {
-            final Properties props = new Properties();
-            props.load(input);
-            input.close();
-            final String propFile = props.getProperty("propertyPath") + props.getProperty("testPropFile");
-            final InputStream authInput = new FileInputStream(propFile);
-            authProp = new Properties(props);
+            authProp=new Properties();
+            InputStream authInput = new FileInputStream(propFile);
             authProp.load(authInput);
-            authInput.close();
+            System.out.println("Props >>"+authProp.toString());
+
         } catch (IOException e) {
-            log.error("initialization error", e);
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
