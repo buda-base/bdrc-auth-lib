a6dd1c1165c58b982581a8517a43269c3ae92c2c
MarcAgate <agate.marc@gmail.com>
2018-10-26 15:14:50 +0200

    Ooops ! Removed old props config

--- src/main/java/io/bdrc/auth/BdrcJwks.java
+++ src/main/java/io/bdrc/auth/BdrcJwks.java
@@ -1,8 +1,6 @@
 package io.bdrc.auth;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.math.BigInteger;
 import java.net.URL;
 import java.security.KeyFactory;
@@ -12,7 +10,6 @@ import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
 import java.util.Base64;
-import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,28 +23,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 /*******************************************************************************
  * Copyright (c) 2018 Buddhist Digital Resource Center (BDRC)
- * 
+ *
  * If this file is a derivation of another work the license header will appear
  * below; otherwise, this work is licensed under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.
- * 
+ *
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * 
+ *
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
 
 public class BdrcJwks {
 
-    static Properties props = new Properties();
-    static Properties authProp = new Properties();
     static JsonNode node;
     static RSAPublicKey publicKey;
     static Algorithm algo;
@@ -65,20 +60,14 @@ public class BdrcJwks {
     public static final String X5T = "x5t";
 
     static {
-        final InputStream input = BdrcJwks.class.getClassLoader().getResourceAsStream("auth.properties");
+
         try {
-            props.load(input);
-            input.close();
-            final InputStream authInput = new FileInputStream(
-                    props.getProperty("propertyPath") + props.getProperty("propertyFile"));
-            authProp.load(authInput);
-            authInput.close();
             final ObjectMapper mapper = new ObjectMapper();
-            final URL url = new URL(authProp.getProperty("jwksUrl"));
+            final URL url = new URL(AuthProps.getProperty("jwksUrl"));
             node = mapper.readTree(url);
             publicKey = buildPublicKey();
             algo = Algorithm.RSA256(publicKey, null);
-            verification = JWT.require(algo).withIssuer(getProp("issuer"));
+            verification = JWT.require(algo).withIssuer(AuthProps.getProperty("issuer"));
         } catch (IOException | CertificateException | InvalidKeySpecException | NoSuchAlgorithmException e) {
             log.error("initialization error", e);
         }
@@ -89,10 +78,6 @@ public class BdrcJwks {
             return node.findValue(X5C).get(0).asText();
         }
         return node.findValue(key).asText();
-    }
-
-    static String getProp(String prop) {
-        return authProp.getProperty(prop);
     }
 
     static RSAPublicKey buildPublicKey()
